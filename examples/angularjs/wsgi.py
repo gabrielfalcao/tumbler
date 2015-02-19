62913a79b76e47e3250fd3b199c50901a26520ef
Gabriel Falcão <gabriel@nacaolivre.org>
2015-02-19 17:49:00 -0500

    adding wsgi conf for angular example

---
+++ examples/angularjs/wsgi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# flake8: noqa
+import os
+from plant import Node
+from buildbottv import routes
+
+from tumbler.core import Web
+
+
+root_node = Node(__file__).dir
+
+application = Web(
+    template_folder=root_node.join('templates'),
+    static_folder=root_node.join('static'),
+    static_url_path='/assets',
+)
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    make_server('', 8000, application).serve_forever()
